ca2612f458a13af8be09551623cff38e4a0c96c5
antofont <94621402+antofont@users.noreply.github.com>
2022-07-01 18:03:09 +0200

    modifiche

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8","mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18", "mySlides19", "mySlides20"]
+let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8","mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18", "mySlides19", "mySlides20", "mySlides21", "mySlides22"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -20,6 +20,8 @@ showSlides(1, 16);
 showSlides(1, 17);
 showSlides(1, 18);
 showSlides(1, 19);
+showSlides(1, 20);
+showSlides(1, 21);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
